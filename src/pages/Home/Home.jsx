eefe6adfdc169fbc37b9c31c8575c935b2d67942
Kaleab Sintayehu <kaleabsintayehu489@gmail.com>
2026-08-09 04:45:36 -0700

    Adding rows

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
+import RowList from "../../components/Rows/RowList/RowsList";
 function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <RowList />
       <Footer />
     </>
   );
